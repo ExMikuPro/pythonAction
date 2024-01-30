1a8422657e073eda0251d3a0695677699a76c2fb
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2024-01-30 16:06:42 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="etest",
+    version="v1.0.39",
+    author="ExMikuPro",
+    author_email="",
+    python_requires=">=3.6.0",
+    packages=find_packages(),
+)
